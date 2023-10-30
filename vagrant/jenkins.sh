abb0bd67c6b1139702c3ccfbbd3db69c7923afb2
Aditya Bhat K <adibhatk96@gmail.com>
2023-10-30 16:56:47 +0530

    update jenkins installation steps

--- vagrant/jenkins.sh
+++ vagrant/jenkins.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "Adding apt-keys"
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+wget -q -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 
 echo "Updating apt-get"
 sudo apt-get -qq update
 
-echo "Installing default-java"
-sudo apt-get -y install default-jre > /dev/null 2>&1
-sudo apt-get -y install default-jdk > /dev/null 2>&1
+echo "Installing fontconfig & openjdk 17"
+sudo apt-get -y install fontconfig openjdk-17-jre > /dev/null 2>&1
 
 echo "Installing git"
 sudo apt-get -y install git > /dev/null 2>&1
@@ -18,7 +19,6 @@ sudo apt-get -y install git-ftp > /dev/null 2>&1
 
 echo "Installing jenkins"
 sudo apt-get -y install jenkins > /dev/null 2>&1
-sudo service jenkins start
 
 sleep 1m
 
